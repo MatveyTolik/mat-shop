5d7964b6d2633d8f81588db9480ac562fcc4d2f9
Amateichuk <amateichuk@deloittece.com>
2026-02-23 09:40:18 +0000

    STRAPI

---
+++ strapi/config/middlewares.ts
@@ -0,0 +1,22 @@
+import type { Core } from '@strapi/strapi';
+
+const config: Core.Config.Middlewares = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:8000'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+export default config;
